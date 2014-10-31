49fb1d744a2709f518dbbea3bbc5201a3fba4033
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-10-31 14:41:44 -0500

    Show survey responses when View Results button is clicked.

--- app/assets/javascripts/show_leader.js
+++ app/assets/javascripts/show_leader.js
@@ -1,5 +1,13 @@
 $('.discussions.show_leader').ready(function(){
 	$('.view-results-button').on('click', function(e) {
-		var id = e.target.id.slice(-2);
+		var id = e.target.id;
+		var numId = id.match(/\d+/)[0];
+		$('.survey-responses-div').hide();
+		$('div#survey-responses-' + numId).show();
+		$('.hide-results-button').show();
+	});
+	$('.hide-results-button').on('click', function() {
+		$('.survey-responses-div').hide();
+		$('.hide-results-button').hide();
 	});
 })
