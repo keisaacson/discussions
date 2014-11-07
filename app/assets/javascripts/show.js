1e10625d89ba99d430dcca84c614979711ea217c
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-11-07 14:42:55 -0600

    Update survey response forms.

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -3,32 +3,33 @@ $('.discussions.show').ready(function(){
 	channel.bind('open_survey', function(data) {
 	  var survey_question = data['survey_question'];
 	  var survey_id = data['id'];
-	  var surveyResponseHtml = '<div id="survey' + survey_id + '"><li>' + survey_question + '</li><form accept-charset="UTF-8" action="/surveys/' + survey_id + '/survey_responses" data-remote="true" method="post"><div style="display:none"><input name="utf8" type="hidden" value="✓"></div><input type="text" id="survey' + survey_id + '-response" name="survey_response[content]"><input type="hidden" name="survey_response[survey_id]" value="' + survey_id + '""></br><input type="submit" value="Submit Response" class="btn btn-xs"></form></div>';
+	  var surveyResponseHtml = '<li><strong>' + survey_question + '</strong></li><div id="survey' + survey_id + '"><form accept-charset="UTF-8" action="/surveys/' + survey_id + '/survey_responses" data-remote="true" method="post" role="form"><div style="display:none"><input name="utf8" type="hidden" value="✓"></div><div class="form-group"><textarea type="text" class="form-control" rows="3" name="survey_response[content]" id="survey' + survey_id + '-response"></textarea></div><input type="hidden" name="survey_response[survey_id]" value="' + survey_id + '""><input type="submit" value="Submit Response" class="btn btn-xs btn-default"></form></div>';
 	  $('p#no-surveys').remove();
 	  $('ul.survey-list').append(surveyResponseHtml);
 	});
 	channel.bind('end_survey', function(data) {
 	  var survey_id = data['id'];
+	  $('div#survey' + survey_id).prev('li').remove();
 	  $('div#survey' + survey_id).remove();
 	});
 	channel.bind('timer', function(data) {
 		var totalSeconds = parseInt(data['seconds']);
 		$('div#' + data['id'] + ' p.timer').remove();
-		$('div#' + data['id']).append('<p class="timer">'+ totalSeconds +' seconds remaining</p>');
+		$('div#' + data['id']).prepend('<p class="timer"></p>');
 		for (var i = totalSeconds; i > 0; i--) {
 			setTimeout(function(x) {
 				return function() { 
 					if (x < 10) {
-						$('div#' + data['id'] + ' p.timer').text('0:0' + String(x));
+						$('div#' + data['id'] + ' p.timer').text('0:0' + String(x) + ' until survey closes');
 					} else if (x < 60) {
-						$('div#' + data['id'] + ' p.timer').text('0:' + String(x));
+						$('div#' + data['id'] + ' p.timer').text('0:' + String(x) + ' until survey closes');
 					} else {
 						var minutes = Math.floor(x/60);
 						var seconds = x % 60;
 						if (seconds < 10) {
-							$('div#' + data['id'] + ' p.timer').text(String(minutes) + ':0' + String(seconds));
+							$('div#' + data['id'] + ' p.timer').text(String(minutes) + ':0' + String(seconds) + ' until survey closes');
 						} else {
-						 $('div#' + data['id'] + ' p.timer').text(String(minutes) + ':' + String(seconds));
+						 $('div#' + data['id'] + ' p.timer').text(String(minutes) + ':' + String(seconds) + ' until survey closes');
 						};
 					};
 				}; 
@@ -38,6 +39,7 @@ $('.discussions.show').ready(function(){
 
 	var channel2 = dispatcher.subscribe('questions');
 	channel2.bind('respond_to_question', function(data) {
+		$('p#no-questions').remove();
 		$('.answered-questions-list').append('<li>' + data['content'] + '</li><p>' + data['response'] + '</p>');
 	});
 })
